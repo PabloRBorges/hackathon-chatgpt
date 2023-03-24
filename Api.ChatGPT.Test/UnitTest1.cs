0777ea418fcd5d8f6e345631e64744f2af103958
Pablo Ramon Borges <pablo@direct.one>
2023-03-24 09:10:43 -0300

    Add project files.

---
+++ Api.ChatGPT.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Api.ChatGPT.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ChatGPTLib/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.WebSockets;
+using Newtonsoft.Json;
+using OpenAI_API;
+
+namespace ChatGPTAPI
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var api = new OpenAIAPI("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");
+            var result = await api.Completions.GetCompletion("One Two Three One Two");
+          //  var teste2 = await api.ImageGenerations.CreateImageAsync("");
+
+            //var chat = api.Chat.CreateConversation();
+
+          //  chat.AppendSystemMessage("Você é um fazendeiro, que passou por um período de seca, qual a melhor data para plantar arroz?");
+
+            
+
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
