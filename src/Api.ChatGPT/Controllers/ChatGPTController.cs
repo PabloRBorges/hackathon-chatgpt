7c243f700f14351ae865aab2873dfda0715f669c
Pablo Ramon Borges <pablo@direct.one>
2023-03-25 03:14:57 -0300

    feat: inclusão da variável de ambiente e um teste de consulta

---
+++ src/Api.ChatGPT/Controllers/ChatGPTController.cs
@@ -0,0 +1,56 @@
+﻿using Core.interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.ChatGPT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatGPTController : ControllerBase
+    {
+        private readonly IChatGPT _chatGPT;
+
+        public ChatGPTController(IChatGPT chatGPT)
+        {
+            _chatGPT = chatGPT;
+        }
+
+        // GET: api/<ChatGPTController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ChatGPTController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            
+
+            return "value";
+        }
+
+        // POST api/<ChatGPTController>
+        [HttpPost]
+        public async Task<string> Post([FromBody] string value)
+        {
+           var result =  await _chatGPT.SendMessageAsync(value);
+
+            return result;
+        }
+
+        // PUT api/<ChatGPTController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ChatGPTController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Core/models/Requests/ChatRequests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.models.Requests
+{
+    public class ChatRequests
+    {
+        public string TenantId { get; set; }
+        public string? TenantName { get; set; }
+
+        public object body { get; set; }
+    }
+}
