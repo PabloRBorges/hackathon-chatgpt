f6436a74ca0e4ff618aebd59480ebb18797205a2
Pablo Ramon Borges <pablo@direct.one>
2023-03-27 01:08:15 -0300

    feat: new file for front

--- src/Core/Adapter/ChatGPTAdapter.cs
+++ src/Core/Adapter/ChatGPTAdapter.cs
@@ -12,8 +12,8 @@ namespace Core.Adapter
 
         public ChatGPTAdapter()
         {
-            _openAIAPI = new OpenAIAPI(Environment.GetEnvironmentVariable(PRIVATE_KEY_CHATGPT));
-            //_openAIAPI = new OpenAIAPI("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");
+            //_openAIAPI = new OpenAIAPI(Environment.GetEnvironmentVariable(PRIVATE_KEY_CHATGPT));
+            _openAIAPI = new OpenAIAPI("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");
         }
 
         public async Task<string> VerifyChatMessages(string message)
